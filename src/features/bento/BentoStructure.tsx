d60d9287aa0e81f71903e2b474bc4ecb473b18e5
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-02-02 00:21:34 +0530

    UI Update

--- src/features/bento/BentoStructure.tsx
+++ src/features/bento/BentoStructure.tsx
@@ -1,6 +1,12 @@
 import { Link, useOutletContext } from "react-router-dom";
+
 import TasksMain from "../task-manager/components/TasksMain";
+import Weather from "../weather/Weather";
 import Pomodoro from "../pomodoro/Pomodoro";
+import Inspire from "../inspire/Inspire";
+import NewsFeed from "../news-feed/NewsFeed";
+import Notes from "../Notes/Notes";
+
 import type { OutletContextType } from "../task-manager/types";
 
 import "./BentoStructure.css";
@@ -13,11 +19,29 @@ const BentoStructure = () => {
         <h1>Welcome to the Dashboard</h1>
       </div>
       <div className="bento-grid-layout">
-        <div className="span-row">
+        <div className="grid-item span-row">
           <TasksMain taskData={tasks} taskSet={setTasks} Type="Priority" />
+          <Link to="../taskmanager">Go</Link>
         </div>
 
-        <Pomodoro />
+        <div className="grid-item">
+          <Weather />
+        </div>
+        <div className="grid-item">
+          <Inspire />
+        </div>
+        <div className="grid-item">
+          <Notes />
+        </div>
+        <div className="grid-item">
+          <Pomodoro />
+        </div>
+        <div className="grid-item span-column">
+          <NewsFeed />
+        </div>
+        <div className="grid-item">
+          <Pomodoro />
+        </div>
       </div>
     </>
   );
---
+++ src/features/inspire/Inspire.tsx
@@ -0,0 +1,5 @@
+const Inspire = () => {
+  return <h1>Inspire</h1>;
+};
+
+export default Inspire;
