783dae3fb7614fb43568aee9532c4d8cb670f49c
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-02-05 23:46:12 +0530

    UI Update

--- src/features/weather/components/Daily.tsx
+++ src/features/weather/components/Daily.tsx
@@ -3,26 +3,42 @@ import RenderAnime from "../hooks/useLottieAnime";
 import type { DailyProps } from "../weatherType";
 import "./Daily.css";
 const Daily = ({ daily, tempUnit }: DailyProps) => {
-  console.log(daily);
-  let dailyData = daily.time.map((day, index) => {
+  const filteredArr = daily.time.filter((day, index) => index > 0);
+  let dailyData = filteredArr.map((day, index) => {
     return (
-      <div className="daily-wrap" key={day}>
-        <div className="daily-date">
-          <p>
-            {new Date(day).toLocaleDateString(undefined, {
-              month: "short",
-              day: "2-digit",
-            })}
-          </p>
+      <div className="daily-main-wrap" key={day}>
+        <div className="daily-weather-wrap">
+          <div className="daily-date">
+            <p>
+              {new Date(day).toLocaleDateString(undefined, {
+                month: "short",
+                day: "2-digit",
+              })}
+            </p>
+          </div>
+          <div className="daily-temp">
+            <p>
+              {daily.temperature_2m_max[index]}
+              {tempUnit}
+            </p>
+          </div>
+          <div className="daily-anime">
+            <RenderAnime code={daily.weather_code[index]} />
+          </div>
         </div>
-        <div className="daily-temp">
-          <p>
-            {daily.temperature_2m_max[index]}
-            {tempUnit}
-          </p>
-        </div>
-        <div className="daily-anime">
-          <RenderAnime code={daily.weather_code[index]} />
+        <div className="daily-sun-wrap">
+          <div className="daily-sun">
+            <p>
+              <strong>Sunrise:</strong>
+              {daily.sunrise[index]}
+            </p>
+          </div>
+          <div className="daily-sun">
+            <p>
+              <strong>Sunset:</strong>
+              {daily.sunset[index]}
+            </p>
+          </div>
         </div>
       </div>
     );
