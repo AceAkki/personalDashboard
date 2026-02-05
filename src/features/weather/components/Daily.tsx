5b085921269ea8342273820e010bc50ae2af5825
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-02-05 23:56:52 +0530

    Daily Feature Update

--- src/features/weather/components/Daily.tsx
+++ src/features/weather/components/Daily.tsx
@@ -5,6 +5,7 @@ import "./Daily.css";
 const Daily = ({ daily, tempUnit }: DailyProps) => {
   const filteredArr = daily.time.filter((day, index) => index > 0);
   let dailyData = filteredArr.map((day, index) => {
+    let newIndex = index + 1;
     return (
       <div className="daily-main-wrap" key={day}>
         <div className="daily-weather-wrap">
@@ -18,25 +19,41 @@ const Daily = ({ daily, tempUnit }: DailyProps) => {
           </div>
           <div className="daily-temp">
             <p>
-              {daily.temperature_2m_max[index]}
+              {daily.temperature_2m_max[newIndex]}
               {tempUnit}
             </p>
           </div>
           <div className="daily-anime">
-            <RenderAnime code={daily.weather_code[index]} />
+            <RenderAnime code={daily.weather_code[newIndex]} />
           </div>
         </div>
         <div className="daily-sun-wrap">
           <div className="daily-sun">
             <p>
-              <strong>Sunrise:</strong>
-              {daily.sunrise[index]}
+              <strong>Sunrise: </strong>
+              {
+                new Date(daily.sunrise[newIndex])
+                  .toLocaleDateString(undefined, {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                  })
+                  .split(", ")[1]
+              }
             </p>
           </div>
           <div className="daily-sun">
             <p>
-              <strong>Sunset:</strong>
-              {daily.sunset[index]}
+              <strong>Sunset: </strong>
+              {
+                new Date(daily.sunset[newIndex])
+                  .toLocaleDateString(undefined, {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                  })
+                  .split(", ")[1]
+              }
             </p>
           </div>
         </div>
