e2d3734b9f0520e98eef99c684b60466449e29a4
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-02-11 23:58:29 +0530

    LinkType

--- src/features/dashboard.tsx
+++ src/features/dashboard.tsx
@@ -4,12 +4,13 @@ import { Header } from "../components/Header";
 import QuickLinks from "../components/QuickLinks";
 
 import type { TaskActionData } from "./task-manager/taskTypes";
+import type { linkObject } from "./linkStorage/linkTypes";
 import "./dashboard.css";
 
 const Dashboard = (): ReactElement => {
   const [tasks, setTasks] = useState<TaskActionData[]>([]);
   const [notes, setNotes] = useState<string[]>([]);
-  const [links, setLinks] = useState<string[]>([]);
+  const [links, setLinks] = useState<linkObject[]>([]);
   const { weatherData, aqiData } = useLoaderData();
   return (
     <>
