947c71c3d820c3844b4479927ffbe1a9615f0c6e
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-05 23:44:22 +0530

    loader up

--- src/features/fallbackElem.tsx
+++ src/features/fallbackElem.tsx
@@ -1,15 +1,17 @@
 import { ThreeCircles } from "react-loader-spinner";
 
-export default function fallBackLoader() {
+export default function FallBackLoader() {
   return (
-    <ThreeCircles
-      visible={true}
-      height="100"
-      width="100"
-      color="#4fa94d"
-      ariaLabel="three-circles-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <section className="loader-wrapper">
+      <ThreeCircles
+        visible={true}
+        height="100"
+        width="100"
+        color="var(--primary)"
+        ariaLabel="three-circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </section>
   );
 }
