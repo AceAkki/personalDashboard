3b7e81a77aadfdb523827b589604a4e8512b8297
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-08-04 23:23:22 +0530

    PWA update

--- src/global/customProto.ts
+++ src/global/customProto.ts
@@ -4,6 +4,12 @@ declare global {
   }
 }
 
+declare global {
+  interface Window {
+    __WB_MANIFEST: any;
+  }
+}
+
 String.prototype.toSentenceCase = function (): string {
   return this.split("")
     .map((char, i) => (i === 0 ? char.toUpperCase() : char))
