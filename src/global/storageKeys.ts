bc454d07a57916e185db8b3992233af2fc662625
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-08-08 23:49:05 +0530

    storage keys addition

--- src/global/storageKeys.ts
+++ src/global/storageKeys.ts
@@ -3,3 +3,4 @@ export const tasksKey = "tasks-storage";
 export const linksKey = "links-storage";
 export const pomoKey = "pomo-storage";
 export const notesKey = "notes-storage";
+export const newsKey = "news-storage";
