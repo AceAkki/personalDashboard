5eca9d3d3f3796d6e3aa0b815f515d12d4772a25
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-08-08 23:45:18 +0530

    up

--- src/features/news-feed/NewsFeed.tsx
+++ src/features/news-feed/NewsFeed.tsx
@@ -1,14 +1,58 @@
+import { useEffect } from "react";
+import { useShallow } from "zustand/shallow";
+
 import useRouteNewsData from "./hooks/useRouteNewsData";
+import { useNewsStore } from "./hooks/useNewsStore";
 import RenderNews from "./components/RenderNews";
+
+import { NewsObject } from "./newsTypes";
 import "./news.css";
+
 const NewsFeed = () => {
-  const newsArr = useRouteNewsData();
+  const { successData, newsArr } = useRouteNewsData();
+
+  const { newsArray, setNewsArray } = useNewsStore(
+    useShallow((state) => ({
+      newsArray: state.newsArray,
+      setNewsArray: state.setNewsArray,
+    })),
+  );
+  useEffect(() => {
+    if (newsArray.length === 0) {
+      setNewsArray(newsArr as NewsObject[]);
+    }
+  }, [newsArray]);
   return (
-    <section className="overflow-auto  inner-route-section">
-      <div className="news-main-wrap">
-        <RenderNews newsArr={newsArr} />
-      </div>
-    </section>
+    <>
+      <section className="overflow-auto  inner-route-section">
+        <button
+          onClick={() => {
+            setNewsArray(newsArr as NewsObject[]);
+          }}
+        >
+          All
+        </button>
+        {Object.keys(successData).map((key) => {
+          return (
+            <button
+              onClick={() => {
+                let categoryNewsArr = Object.values(
+                  successData[key as keyof typeof successData],
+                ).flat();
+                setNewsArray(categoryNewsArr as NewsObject[]);
+              }}
+            >
+              {key}
+            </button>
+          );
+        })}
+      </section>
+      <section className="overflow-auto  inner-route-section">
+        <div className="news-main-wrap">
+          <RenderNews newsArr={newsArray} />
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/features/news-feed/hooks/useRouteNewsData.tsx
+++ src/features/news-feed/hooks/useRouteNewsData.tsx
@@ -1,10 +1,35 @@
 import { useRouteLoaderData } from "react-router-dom";
-import type { NewsFeedProps } from "../newsTypes";
+import type { NewsFeedProps, NewsSourceData } from "../newsTypes";
+
 const useRouteNewsData = () => {
   const { newsData } = useRouteLoaderData("root") as NewsFeedProps;
-  const successDataArr = newsData.filter((news) => news.status === "ok");
-  const newsArr = successDataArr.map((news) => news.items).flat();
-  return newsArr;
+  console.log(newsData);
+  const successData = {};
+  for (let [key, value] of Object.entries(newsData) as NewsSourceData) {
+    let valueData = {};
+    for (const [subkey, subValue] of Object.entries(value)) {
+      if (subValue.status === "ok") {
+        Object.defineProperty(valueData, subkey, {
+          value: subValue.items,
+          enumerable: true,
+          writable: true,
+        });
+      }
+    }
+    Object.defineProperty(successData, key, {
+      value: valueData,
+      enumerable: true,
+      writable: true,
+    });
+  }
+  // const successDataArr = newsData.filter((news) => news.status === "ok");
+  // const newsArr = successDataArr.map((news) => news.items).flat();
+  console.log(successData);
+  const newsArr = Object.values(successData)
+    .map((objValue) => Object.values(objValue).flat())
+    .flat();
+  console.log(newsArr);
+  return { successData, newsArr };
 };
 
 export default useRouteNewsData;
