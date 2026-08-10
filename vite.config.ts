974b04fc98be68829b8e29fc3fa2781cf92e8da6
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-08-10 23:51:16 +0530

    pwa update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["src/assets/android/launchericon-512x512.png"],
+      manifest: {
+        name: "My React PWA",
+        short_name: "ReactPWA",
+        start_url: "/",
+        description: "Next-gen React PWA with automatic memoization",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "src/assets/android/launchericon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "src/assets/android/launchericon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"], // Cache all build assets
+      },
+    }),
+  ],
 });
