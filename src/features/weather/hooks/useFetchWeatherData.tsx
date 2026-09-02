3187022af8306d685c7a2d1aa8a81b0f961d79e5
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-02 22:46:25 +0530

    hook update

--- src/features/weather/hooks/useFetchWeatherData.tsx
+++ src/features/weather/hooks/useFetchWeatherData.tsx
@@ -84,11 +84,11 @@ const useFetchWeatherData = () => {
       ? { weatherData: weatherQuery.data, aqiData: aqiQuery.data }
       : { weatherData: weatherStoreData, aqiData: aqiStoreData };
 
-  console.log(
-    weatherStorageTime,
-    aqiStorageTime,
-    weatherStorageTime > getNewTime(),
-  );
+  // console.log(
+  //   weatherStorageTime,
+  //   aqiStorageTime,
+  //   weatherStorageTime > getNewTime(),
+  // );
   return {
     ...dataConfig,
     weatherQueryLoading: weatherQuery.isLoading,
